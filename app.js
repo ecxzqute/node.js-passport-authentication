25a2a324a6e833a67402c014d8d07b65ed374eec
Ja Bune <jabune888@gmail.com>
2019-05-09 20:14:28 +0800

    added index and users routes

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// Routes
+app.use('/', require('./routes/index'));
+
+app.use('/users', require('./routes/users'));
+
 const PORT = process.env.PORT || 5000;
 
 
